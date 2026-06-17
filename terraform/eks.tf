b695438437108f239234909175f6b304ba79dc1d
seetha <seethalakshmikap@gmail.com>
2026-06-17 16:01:54 +0530

    Initial EKS ArgoCD GitOps setup

---
+++ terraform/eks.tf
@@ -0,0 +1,36 @@
+module "eks" {
+
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.37.2"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.30"
+
+  cluster_endpoint_public_access = true
+
+  vpc_id = module.vpc.vpc_id
+
+  subnet_ids = module.vpc.private_subnets
+
+  enable_cluster_creator_admin_permissions = true
+
+  enable_irsa = true
+
+  eks_managed_node_groups = {
+
+    default = {
+
+      desired_size = 2
+      min_size     = 1
+      max_size     = 2
+
+      instance_types = ["t3.small"]
+
+      capacity_type = "ON_DEMAND"
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
